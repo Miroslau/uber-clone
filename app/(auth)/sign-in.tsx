018d580de4458bbebbf0448408acc23fbb7f6edb
Miroslau <forse2408@gmail.com>
2024-08-18 12:31:14 +0300

    feature: add new onboarding screen

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+const SignUp = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignUp</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
